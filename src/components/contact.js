bcb2eacaabbbed64a7a0042c4b74715de6326306
edina do nascimento silva <edinas09@gmail.com>
2020-09-08 12:10:59 -0700

    The Edina Silva Portfolio was update

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,7 +12,7 @@ class Contact extends Component {
             </h2>
             <img className="avatar-img-contact" src="https://avatars3.githubusercontent.com/u/38966838?s=400&u=bed3cbddac22922017a360e93085f741b1d5fd66&v=4" alt="avatar" />
             
-            <p style={{ width: '75%', margin: 'auto', paddingTop: '3em'}}>
+            <p style={{ width: '80%', margin: 'auto', paddingTop: '3em'}}>
               Hi, my name is Edina. I was born in São Paulo, Brazil, where I lived most of my life. However, I decided to move to Vancouver and did a web & app development college. As of 2019, I'm living in Bellevue-WA.
             </p>
 
@@ -27,14 +27,14 @@ class Contact extends Component {
 
 
                 <ListItem>
-                  <ListItemContent style={{fontsize: '25px', fontFamily: 'Anton',  color: '#fff'}}>
+                  <ListItemContent style={{fontsize: '1.2rem', fontFamily: 'Anton',  color: '#fff'}}>
                     <i className="fa fa-envelope" aria-hidden="true"/>
                       edinas09@gmail.com
                       </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontsize: '25px', fontFamily: 'Anton' ,  color: '#fff'}}>
+                  <ListItemContent style={{fontsize: '1.2rem', fontFamily: 'Anton' ,  color: '#fff'}}>
                     <i className="fa fa-skype" aria-hidden="true"/>
                       edinas09
                       </ListItemContent>
